48e292b4a382a2c6ee6c61757a4024d09c8c4daf
Kai Kousa <kai.kousa@gmail.com>
2015-03-02 13:37:52 +0200

    Improved subscription.

--- api/subscribe-instagram.sh
+++ api/subscribe-instagram.sh
@@ -1,9 +1,26 @@
 
 
-curl -F 'client_id=' \
-     -F 'client_secret=' \
+curl -F 'client_id=${INSTAGRAM_CLIENT_ID}' \
+     -F 'client_secret=${INSTAGRAM_CLIENT_SECRET}' \
      -F 'object=tag' \
      -F 'aspect=media' \
      -F 'object_id=northernlight' \
      -F 'callback_url=http://auroragram.com/api/v1/instagram/' \
      https://api.instagram.com/v1/subscriptions/
+
+curl -F 'client_id=${INSTAGRAM_CLIENT_ID}' \
+     -F 'client_secret=${INSTAGRAM_CLIENT_SECRET}' \
+     -F 'object=tag' \
+     -F 'aspect=media' \
+     -F 'object_id=auroraborealis' \
+     -F 'callback_url=http://auroragram.com/api/v1/instagram/' \
+     https://api.instagram.com/v1/subscriptions/
+
+curl -F 'client_id=${INSTAGRAM_CLIENT_ID}' \
+     -F 'client_secret=${INSTAGRAM_CLIENT_SECRET}' \
+     -F 'object=tag' \
+     -F 'aspect=media' \
+     -F 'object_id=northernlights' \
+     -F 'callback_url=http://auroragram.com/api/v1/instagram/' \
+     https://api.instagram.com/v1/subscriptions/
+
